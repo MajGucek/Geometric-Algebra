ab468115badcc37a677adc43d70878d71ec4a0bf
MajGucek <113456426+MajGucek@users.noreply.github.com>
2025-04-17 21:02:42 +0200

    small tweaks

--- CGA_2D.js
+++ CGA_2D.js
@@ -14,20 +14,26 @@ Algebra(3, 1, () => {
     let ei = 1e4 + 1e3;
     let eo = 0.5e4 - 0.5e3;
     /* End of Null vectors section */
+    /* Helper Functions */
+    let time = () => performance.now() / 1000;
+    /* End of Helper Functions section */
     /* Templates for creating GA objects */
     let create_point = (x, y) => eo + x * 1e1 + y * 1e2 + 0.5 * (x * x + y * y) * ei;
     let create_line = (a, b, c) => !(a * 1e1 + b * 1e2 + c * ei);
-    let create_circle = (x, y, r) => !(point(x, y) - (r ** 2 / 2) * ei);
+    let create_circle = (x, y, r) => !(create_point(x, y) - (r ** 2 / 2) * ei);
     let dist = (x, y) => (2 * (x << y).Length) ** 0.5;
     let angle = (x, y) => Math.acos(!x.Normalized << !y.Normalized);
+    let translate = (v) => (1 - 0.5 * v) ^ ei;
+    let rotate = (P, a) => Math.cos(a / 2) - Math.sin(a / 2) * (((1e12 - P) << 1e12) ^ ei);
     /* End of templates section */
 
-    let p1 = create_point(1, 1);
+    let p1 = create_point(1, -1);
     let p2 = create_point(-1, -1);
-    let p3 = create_point(-1, 1.5);
-    addToRender(p1, p2, p3, () => p1 ^ p2 ^ ei);
-    addToRender(() => p1 ^ p2 ^ p3);
-    addToRender(() => [p3, p1]);
+    let p3 = create_point(1, 1);
+
+    addToRender(p1, "1", p2, "2");
+
+    addToRender((rotate(p1, time()) * translate(Math.sin(time()) * 1e1)) >>> p2);
 
     document.body.appendChild(
         this.graph(
